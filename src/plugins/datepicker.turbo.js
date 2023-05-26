037e80f15d90b142ea221571617ccdee428c3462
Evandro dos Santos <129754540+evandrosl-hisofi@users.noreply.github.com>
2023-05-26 18:51:32 +0000

    fix: multiple rerender turbo

--- src/plugins/datepicker.turbo.js
+++ src/plugins/datepicker.turbo.js
@@ -4,10 +4,8 @@ import DateRangePicker from 'flowbite-datepicker/DateRangePicker';
 import Events from '../dom/events';
 
 const turboLoadEvents = new Events('turbo:load', [initDatepickers]);
-const turboFrameLoadEvents = new Events('turbo:frame-load', [initDatepickers]);
 
 turboLoadEvents.init();
-turboFrameLoadEvents.init();
 
 export default {
     Datepicker,
